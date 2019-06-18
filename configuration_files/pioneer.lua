34343f17f6f8cec1003e0ec6796785913be6cdf2
Daniel Koch <daniel.koch@stud.hs-mannheim.de>
2019-06-18 15:50:26 +0200

    Add imu support

--- configuration_files/pioneer.lua
+++ configuration_files/pioneer.lua
@@ -30,6 +30,6 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
-TRAJECTORY_BUILDER_2D.use_imu_data=false
+TRAJECTORY_BUILDER_2D.use_imu_data=true
 
 return options
